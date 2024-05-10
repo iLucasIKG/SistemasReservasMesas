51281453eb162ef62fe1627144b08c910755c42f
Mariannorio <rodriguezmarianoarg@gmail.com>
2024-05-10 19:59:33 -0300

    mesa vista

--- views/mesa.php
+++ views/mesa.php
@@ -6,6 +6,17 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form method="POST" action="{{ route('procesarFormulario') }}" class="form-control">
+        @csrf
+        <div class="mb-3">
+            <label for="provincia">Provincia:</label>
+            <input type="text" name="provincia" placeholder="Nombre de la Provincia" value="{{ $provincia ?? '' }}" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="localidades">Localidades (separadas por coma):</label>
+            <input type="text" name="localidades" placeholder="Localidad 1, Localidad 2, ..." value="{{ $localidades ?? '' }}">
+        </div>
+        <input type="submit" value="Enviar" class="btn btn-primary">
+    </form>    
 </body>
 </html>
