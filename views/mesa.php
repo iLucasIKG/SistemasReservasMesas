770d496d63596ba1073c6e4cb195a3dc31cd5bfb
Mariannorio <rodriguezmarianoarg@gmail.com>
2024-05-10 20:11:09 -0300

    se une header y footer a mesa

--- views/mesa.php
+++ views/mesa.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php include'header.php'?>
+
     <form method="POST" action="{{ route('procesarFormulario') }}" class="form-control">
-        @csrf
         <div class="mb-3">
             <label for="provincia">Provincia:</label>
             <input type="text" name="provincia" placeholder="Nombre de la Provincia" value="{{ $provincia ?? '' }}" class="form-control">
@@ -17,6 +10,6 @@
             <input type="text" name="localidades" placeholder="Localidad 1, Localidad 2, ..." value="{{ $localidades ?? '' }}">
         </div>
         <input type="submit" value="Enviar" class="btn btn-primary">
-    </form>    
-</body>
-</html>
+    </form>
+    
+<?php include'footer.php'?>
