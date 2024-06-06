fa179d108793a125da9d7ebda7037182b05d5ed8
Mariannorio <rodriguezmarianoarg@gmail.com>
2024-06-06 20:06:30 -0300

    se elimina store.php

--- views/reservas/store.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once '../../controllers/controller.reservas.php';
-
-    $obj = new ReservasController();
-    $obj->realizarReserva($_POST['nombreCompleto'],$_POST['email'],$_POST['cantidadPersonas'],$_POST['fechaReservada'],$_POST['horarioReservado']);
-
-?>
